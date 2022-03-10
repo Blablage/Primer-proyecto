d202ce2ca1f47d3a14860b5f0fe601e2e5a0f48a
Blablage <95836461+Blablage@users.noreply.github.com>
2022-03-09 23:22:18 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//Vista tentativa de los datos en firebase
 let database = [
     {name: "Gerardo Garcia", course: "Computacion I", startDate: "30-1-2022", endDate: "30-1-2022"},
     {name: "Monica Dominguez", course: "Ingles C1", startDate: "25-9-2021", endDate: "25-9-2021"},
@@ -5,31 +6,47 @@ let database = [
     {name: "Omar Aguilar", course: "Natacion", startDate: "15-12-2021", endDate: "15-12-2021"},
   ];
 
-//por probar
-  function buildTable(data) {
-    var table = document.createElement("table");
+//Modelo Tentativo por corregir
+function buildTable(data) {
   
-    var fields = Object.keys(data[0]);
-    var headRow = document.createElement("tr");
-    fields.forEach(function(field) {
-      var headCell = document.createElement("th");
-      headCell.appendChild(document.createTextNode(field));
-      headRow.appendChild(headCell);
-    });
-    table.appendChild(headRow);
-  
-    data.forEach(function(object) {
-      var row = document.createElement("tr");
-      fields.forEach(function(field) {
-        var cell = document.createElement("td");
-        cell.appendChild(document.createTextNode(object[field]));
-        if (typeof object[field] == "number") {
-          cell.style.textAlign = "right";
-        }
-        row.appendChild(cell);
-      });
-      table.appendChild(row);
-    });
-  
-    return table;
+  document.getElementById('databaseTable').innerHTML = `
+  <tr>
+    <th scope="row">1</th>
+    <td>Gerardo García</td>
+    <td>Computación Nivel 1</td>
+    <td>10</td>
+    <td>1/12/2021</td>
+    <td>30/1/2022</td>
+  <td class="d-flex">
+    <a class="ps-2" href="#"><img src="images/Plus.png"/></a>
+    <a href="#"><img src="images/Minus.png"/></a>
+  </td>
+  </tr>
+  <tr>
+    <th scope="row">2</th>
+    <td>David López</td>
+    <td>Cocina A</td>
+    <td>8</td>
+    <td>1/12/2021</td>
+    <td>30/1/2022</td>
+    <td class="d-flex">
+      <a class="ps-2" href="#"><img src="images/Plus.png"/></a>
+      <a href="#"><img src="images/Minus.png"/></a>
+    </td>
+  </tr>
+  <tr>
+    <th scope="row">3</th>
+    <td>Cristina Dominguez</td>
+    <td>Administración</td>
+    <td>9</td>
+    <td>1/12/2021</td>
+    <td>30/1/2022</td>
+    <td class="d-flex">
+      <a class="ps-2" href="#"><img src="images/Plus.png"/></a>
+      <a href="#"><img src="images/Minus.png"/></a>
+    </td>
+  </tr>
+  `
+
+
 }
